1503f3cca1d338f5dfba749f7f32e5d89d13ea5e
obryl <olehbr29@gmail.com>
2022-12-28 12:31:17 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# random-team-generator
+
+## What?
+
+A simple, easy to use, webbased team generator.
+It allows to build totally random or skill based (but still kinda random) teams.
+
+## Why?
+
+I use this project to learn web development. And I wanted to do something, that might be useful to others too.
---
+++ script.js
@@ -0,0 +1,225 @@
+let players = [];
+
+const onStart = () => {
+  hidepList(players);
+  displayList(players, "playerList");
+};
+
+// Add new Player to the array.
+const addPlayer = () => {
+  // Check if playername is empty.
+  if (document.getElementById("player").value === "") {
+    // Set focus on textfield.
+    document.getElementById("player").focus();
+    return false;
+  } else {
+    const newPlayer = {
+      name: document.getElementById("player").value,
+      skill: Number(document.getElementById("skill").value),
+    };
+
+    players.push(newPlayer);
+    document.getElementById("player").value = "";
+    document.getElementById("skill").value = 1;
+
+    // Hide list if no players are there to display.
+    hidepList();
+
+    // Display a list of players.
+    displayList(players, "playerList");
+
+    document.getElementById("player").focus();
+
+    // Don't reload the page on submit.
+    return false;
+  }
+};
+
+// Allow enter key to submit a new player everywhere on the page.
+document.addEventListener("keypress", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("submit").click();
+  }
+});
+
+// Add one to the number of teams.
+const addNumberOfTeams = () => {
+  let currentNumberOfTeams = Number(
+    document.getElementById("numberOfTeams").value
+  );
+  if (currentNumberOfTeams !== 10) {
+    currentNumberOfTeams += 1;
+    document.getElementById("numberOfTeams").value = currentNumberOfTeams;
+  }
+  return false;
+};
+
+// Subtract one from the number of teams.
+const subNumberOfTeams = () => {
+  let currentNumberOfTeams = Number(
+    document.getElementById("numberOfTeams").value
+  );
+  if (currentNumberOfTeams !== 1) {
+    currentNumberOfTeams -= 1;
+    document.getElementById("numberOfTeams").value = currentNumberOfTeams;
+  }
+  return false;
+};
+
+// Shuffle the player array.
+const shuffle = () => {
+  let currentIndex = players.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // and swap it with the current element.
+    temporaryValue = players[currentIndex];
+    players[currentIndex] = players[randomIndex];
+    players[randomIndex] = temporaryValue;
+  }
+
+  // Return the shuffled player array.
+  return players;
+};
+
+// Create balanced teams based on the skilllevel.
+const balancedTeams = () => {
+
+  // Get the number of teams.
+  let numberOfTeams = document.getElementById("numberOfTeams").value;
+  let teams = new Array(parseInt(numberOfTeams)).fill(null).map(item => []);
+
+  // Sort the player array from strong to weak based of skill.
+  const toShuffle = shuffle(players).sort(
+    (a, b) => parseFloat(b.skill) - parseFloat(a.skill)
+  );
+
+
+  toShuffle.forEach((player, index) => teams[index % numberOfTeams].push(player));
+  
+
+  // Hide list if no teams are there to display.
+  hidetList(teams);
+
+  // Display a list of the teams.
+  displayList(teams, "teamList");
+
+  // Reset the teams array.
+  teams = [];
+};
+
+// Function to display a list of the players and teams.
+const displayList = (arr, list) => {
+  let i = 1;
+  let result = "";
+  if (arr === players) {
+    arr.forEach(function (item) {
+      result += `<div class="listItem">
+        <li>
+          <span class="skill liststyle">${item.skill}</span>
+          <span class="name liststyle">${item.name}</span>
+          <button class="delete liststyle" id="${item.name}" onclick="deletePlayer('${item.name}')">
+            <i class="fas fa-trash-alt"></i>
+          </button>
+        </li>
+      </div>`;
+    });
+  } else {
+    arr.forEach(function (item) {
+      elementS = [];
+      elementP = "";
+      item.forEach(function (object) {
+        if (object.teamSkill) {
+          elementS += `<span class="skill green teamskills liststyle"> ${object.teamSkill} </span>`;
+          // return false;
+        } else {
+          elementP += `<span class="name liststyle">${object.name} ${object.skill}</span>`;
+        }
+      });
+      result += `<div class="listItem teams">
+        <li>
+          <div class="row">
+            <span class="number liststyle"> ${i} </span>
+            ${elementS}
+          </div>
+          <div class="team liststyle"> ${elementP} </div> 
+        </li>
+      </div>`;
+      i++;
+    });
+  }
+  document.getElementById(list).innerHTML = result;
+};
+
+// Delete the player from the list and array.
+const deletePlayer = (id) => {
+  const object = document.querySelector(`#${id}`);
+  const index = players
+    .map(function (e) {
+      return e.name;
+    })
+    .indexOf(id);
+  if (index > -1) {
+    players.splice(index, 1);
+  }
+
+  // Remove object from the list.
+  object.parentNode.remove();
+
+  displayList(players, "playerList");
+};
+
+// Split shuffled player array into set sized teams.
+const randomTeams = () => {
+  let teams = [];
+  let toShuffle = shuffle(players).slice();
+
+  // Get the number of teams.
+  let numberOfTeams = document.getElementById("numberOfTeams").value;
+
+  // Split the players array into set amount of teams.
+  while (toShuffle.length) {
+    const teamSize = Math.ceil(toShuffle.length / numberOfTeams--);
+    const team = toShuffle.slice(0, teamSize);
+    teams.push(team);
+    toShuffle = toShuffle.slice(teamSize);
+  }
+
+  // Hide list if no teams are there to display.
+  hidetList(teams);
+
+  // Display a list of the teams.
+  displayList(teams, "teamList");
+
+  // Reset the teams array.
+  teams = [];
+};
+
+/* Work in progress */
+// Check if there are players to display, if not, hide the list element.
+const hidepList = (players) => {
+  let pList = document.getElementById("pList");
+
+  if (players) {
+    pList.style.display = "none";
+  } else {
+    pList.style.display = "block";
+  }
+};
+
+// Check if there are teams to display, if not, hide the list element.
+const hidetList = (teams) => {
+  let tList = document.getElementById("tList");
+  if (teams !== 0) {
+    tList.style.display = "block";
+  } else if (teams === 0) {
+    tList.style.display = "none";
+  }
+};
